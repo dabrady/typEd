a630e17eb6a63faf33dcbcab9fa13042919edf22
Daniel Brady <dabrady@indiana.edu>
2014-07-07 22:50:00 -0400

    hello world!

--- gui/Editor.java
+++ gui/Editor.java
@@ -1,10 +1,15 @@
+import javax.swing.*;
+
 /**
  * Created by Daniel Brady as part of the typEd project.
  * Last modified on 07 July 2014 at 10:23 PM.
- * .
- * This class ....
+ *
+ * This class models a simple text editor.
  */
 
 public class Editor {
 
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog( null, "Hello, world!" );
+    }
 }
